51f35b0c5bcf7c60fcece889b25d06cacb750739
Tony <toneyobaze@gmail.com>
2022-02-13 08:23:13 +0000

    create Main screen

---
+++ src/screens/Main.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Main = () => {
+  return (
+    <View>
+      <Text>Main</Text>
+    </View>
+  );
+};
+
+export default Main;
+
+const styles = StyleSheet.create({});
