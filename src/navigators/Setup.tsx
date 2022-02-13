f17d6f0e098b341a6bc8cc185ca572224fe6a300
Tony <toneyobaze@gmail.com>
2022-02-13 13:23:56 +0000

    setup dynamic initial route

--- src/navigators/Setup.tsx
+++ src/navigators/Setup.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SetupStackParamList } from '../types/navigation';
+import { useSelector } from 'react-redux';
+import { ReducerTypes } from '../types/reducerTypes';
+import Welcome from '../screens/setup/Welcome';
+import SignIn from '../screens/setup/SignIn';
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<SetupStackParamList>();
 
 const Setup = () => {
+  const { firstRun } = useSelector(({ pref }: ReducerTypes) => pref);
+
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Setup" component={SetupScreen} />
+    <Stack.Navigator
+      initialRouteName={firstRun ? 'Welcome' : 'SignIn'}
+      screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Welcome" component={Welcome} />
+      <Stack.Screen name="SignIn" component={SignIn} />
     </Stack.Navigator>
   );
-};
-
-const SetupScreen = () => {
-  return <></>;
 };
 
 export default Setup;
