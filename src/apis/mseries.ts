b1d4ce820d36cbdd76fe6d8927ce1c0446d26750
Tony <toneyobaze@gmail.com>
2022-02-15 22:22:51 +0000

    add language param to mseries global config

--- src/apis/mseries.ts
+++ src/apis/mseries.ts
@@ -1,1 +1,11 @@
-export const mseries = '';
+import axios from 'axios';
+import * as RNLocalize from 'react-native-localize';
+
+const mseries = axios.create({
+  baseURL: 'https://mseries.herokuapp.com/api/v1',
+  params: {
+    language: RNLocalize.getLocales()[0].languageTag,
+  },
+});
+
+export default mseries;
