e90799f352f2cb50d6676ad85b4b7334611f09d8
Tony <toneyobaze@gmail.com>
2022-02-16 21:22:31 +0000

    add actions to pref actions

--- src/actions/pref.ts
+++ src/actions/pref.ts
@@ -4,3 +4,24 @@ export const setFirstRun = (firstRun: boolean) => {
     payload: firstRun,
   };
 };
+
+export const setSetupComplete = (setupComplete: boolean) => {
+  return {
+    type: 'SET_SETUP_COMPLETE',
+    payload: setupComplete,
+  };
+};
+
+export const setIsAuthenticated = (isAuthenticated: boolean) => {
+  return {
+    type: 'SET_IS_AUTHENTICATED',
+    payload: isAuthenticated,
+  };
+};
+
+export const setIsNewUser = (isNewUser: boolean | undefined) => {
+  return {
+    type: 'SET_IS_NEW_USER',
+    payload: isNewUser,
+  };
+};
