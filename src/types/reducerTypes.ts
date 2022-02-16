4f707f76777e7aabd58bf906720d99c8082bfcdc
Tony <toneyobaze@gmail.com>
2022-02-16 21:21:37 +0000

    create ShowReducer and Genre types

--- src/types/reducerTypes.ts
+++ src/types/reducerTypes.ts
@@ -2,12 +2,26 @@ import { FirebaseAuthTypes } from '@react-native-firebase/auth';
 export interface ReducerTypes {
   pref: PrefReducer;
   auth: AuthReducer;
+  show: ShowReducer;
 }
 
 export interface PrefReducer {
   firstRun: boolean;
+  setupComplete: boolean;
+  isAuthenticated: boolean;
+  isNewUser: boolean | undefined;
 }
 
 export interface AuthReducer {
   user: FirebaseAuthTypes.User | null;
+}
+
+export interface ShowReducer {
+  genres: Genre[];
+  userGenres: Genre[];
+}
+
+export interface Genre {
+  id: number;
+  name: string;
 }
