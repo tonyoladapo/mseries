dd44cfb1f90583b88db1c1c1177680ded300d6ff
Tony <toneyobaze@gmail.com>
2022-02-16 21:23:27 +0000

    create pref actions

--- src/reducers/pref.ts
+++ src/reducers/pref.ts
@@ -1,5 +1,8 @@
 const initial_state = {
   firstRun: true,
+  setupComplete: false,
+  isAuthenticated: false,
+  isNewUser: true,
 };
 
 interface ActionTypes {
@@ -11,6 +14,15 @@ const prefReducer = (state = initial_state, { type, payload }: ActionTypes) => {
   switch (type) {
     case 'SET_FIRST_RUN':
       return { ...state, firstRun: payload };
+
+    case 'SET_SETUP_COMPLETE':
+      return { ...state, setupComplete: payload };
+
+    case 'SET_IS_AUTHENTICATED':
+      return { ...state, isAuthenticated: payload };
+
+    case 'SET_IS_NEW_USER':
+      return { ...state, isNewUser: payload };
 
     default:
       return state;
