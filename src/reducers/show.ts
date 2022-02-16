cb6484552efab1daee507e6ff99ae3c7411ac99b
Tony <toneyobaze@gmail.com>
2022-02-16 21:20:50 +0000

    create show reducer

---
+++ src/reducers/show.ts
@@ -0,0 +1,24 @@
+const initial_state = {
+  genres: [],
+  userGenres: [],
+};
+
+interface ActionTypes {
+  type: string;
+  payload?: any;
+}
+
+const showReducer = (state = initial_state, { type, payload }: ActionTypes) => {
+  switch (type) {
+    case 'SET_GENRES':
+      return { ...state, genres: payload };
+
+    case 'SET_USER_GENRES':
+      return { ...state, userGenres: payload };
+
+    default:
+      return state;
+  }
+};
+
+export default showReducer;
