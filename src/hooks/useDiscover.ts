e8af8ae8ddfa0ded206165e0bac6eea91c1deb1f
Tony <toneyobaze@gmail.com>
2022-02-16 21:44:31 +0000

    pass user genres to discover request

--- src/hooks/useDiscover.ts
+++ src/hooks/useDiscover.ts
@@ -1,23 +1,30 @@
+import { ReducerTypes } from './../types/reducerTypes';
 import mseries from '../apis/mseries';
+import { useSelector } from 'react-redux';
 
 const useDiscover = () => {
+  const { userGenres } = useSelector(({ show }: ReducerTypes) => show);
+
   const getDiscoverShows = async () => {
     try {
-      const genres = [
-        { id: '16', name: 'Animation' },
-        { id: '35', name: 'Comedy' },
-        { id: '10759', name: 'Action & Adventure' },
-      ];
-
       const similarShowIds = [
         { id: '60059', name: 'Better Call Saul' },
         { id: '1399', name: 'Game of Thrones' },
         { id: '60625', name: 'Rick and Morty' },
       ];
 
+      const genreParams =
+        userGenres.length > 0
+          ? userGenres
+          : [
+              { id: '16', name: 'Animation' },
+              { id: '35', name: 'Comedy' },
+              { id: '10759', name: 'Action & Adventure' },
+            ];
+
       const { data } = await mseries.get('/discover', {
         params: {
-          genres: JSON.stringify(genres),
+          genres: JSON.stringify(genreParams),
           similar_ids: JSON.stringify(similarShowIds),
         },
       });
