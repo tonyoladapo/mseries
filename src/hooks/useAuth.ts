abf2c6d68d0443935bf234df166582838283d901
Tony <toneyobaze@gmail.com>
2022-02-18 08:44:30 +0000

    clear discoverShows array on sign out

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -3,6 +3,7 @@ import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import { useDispatch } from 'react-redux';
 import { setIsAuthenticated, setIsNewUser } from '../actions/pref';
+import { setDiscoverShows } from '../actions/discover';
 //@ts-ignore
 import { IOS_CLIENT_ID, WEB_CLIENT_ID } from 'react-native-dotenv';
 
@@ -47,6 +48,7 @@ const useAuth = () => {
   const signOut = async () => {
     try {
       await auth().signOut();
+      dispatch(setDiscoverShows([]));
       dispatch(setIsAuthenticated(false));
     } catch (error) {
       console.error(error);
