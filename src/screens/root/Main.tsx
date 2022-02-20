7fef80beaad852cc58a19334ad85609de7b7bb31
Tony <toneyobaze@gmail.com>
2022-02-20 12:00:05 +0000

    fetch user genres from firebase

--- src/screens/root/Main.tsx
+++ src/screens/root/Main.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { setUserGenres } from '../../actions/show';
+import docRef from '../../firebase/docRef';
 import BottomTab from '../../navigators/BottomTab';
 
 const Main = () => {
+  const dispatch = useDispatch();
+  const { userDataRef } = docRef();
+
+  useEffect(() => {
+    const regGenresSnapshot = async () => {
+      try {
+        userDataRef.collection('user_genres').onSnapshot(querySnapshot => {
+          if (!querySnapshot) return;
+
+          const data: any[] = [];
+
+          querySnapshot.forEach((doc: any) => {
+            data.push(doc.data());
+          });
+
+          dispatch(setUserGenres(data));
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    regGenresSnapshot();
+  }, []);
+
   return <BottomTab />;
 };
 
