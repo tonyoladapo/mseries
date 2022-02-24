f989966cc1bffa411c4e43a16798e7e1408ff38e
Tony <toneyobaze@gmail.com>
2022-02-24 15:20:44 +0000

    move ShowDetails to root folder

---
+++ src/screens/root/ShowDetails.tsx
@@ -0,0 +1,66 @@
+import React, { useEffect } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ActivityIndicator,
+  ScrollView,
+} from 'react-native';
+import { useSelector } from 'react-redux';
+import { ReducerTypes } from '../../types/reducerTypes';
+import useShow from '../../hooks/useShow';
+
+const ShowDetails = (props: any) => {
+  const { loading, added, seasons } = useSelector(
+    ({ showDetails }: ReducerTypes) => showDetails,
+  );
+
+  const controller = new AbortController();
+
+  const show = props.route.params.show;
+  const showId = show.id.toString();
+
+  const { addShow, removeShow, getShowDetails, checkAdded, resetState } =
+    useShow(controller);
+
+  useEffect(() => {
+    checkAdded(showId);
+    return () => resetState();
+  }, []);
+
+  useEffect(() => {
+    added && getShowDetails(showId);
+  }, [added]);
+
+  return (
+    <ScrollView style={styles.container}>
+      <Text>{show.name}</Text>
+      {loading ? (
+        <ActivityIndicator style={{ padding: 16 }} />
+      ) : (
+        <TouchableOpacity
+          style={{ padding: 16 }}
+          onPress={() => (added ? removeShow(showId) : addShow(show))}>
+          <Text>{added ? 'Remove show' : 'Add show'}</Text>
+        </TouchableOpacity>
+      )}
+
+      {seasons.length > 0 &&
+        seasons.map(
+          season =>
+            season.season_number > 0 && (
+              <View key={season.id}>
+                <Text>{`Season ${season.season_number}`}</Text>
+              </View>
+            ),
+        )}
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {},
+});
+
+export default ShowDetails;
