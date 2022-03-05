0665cfb3448a6c7f80fea4e10ffae819bac48fbd
Tony <toneyobaze@gmail.com>
2022-03-05 00:09:45 +0000

    create color values

---
+++ src/values/colors.ts
@@ -0,0 +1,10 @@
+export = {
+  primaryBackground: '#000000',
+  primaryGreen: '#64D699',
+  primaryRed: '#F0535D',
+  primaryYellow: '#FEC542',
+  secondaryBackground: '#292929',
+  darkGreen: '#286053',
+  primaryText: '#FFFFFF',
+  mutedText: '#777777',
+};
