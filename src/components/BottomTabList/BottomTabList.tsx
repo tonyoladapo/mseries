c055304273a14b6616c54d84011987ca9c24a060
Tony <toneyobaze@gmail.com>
2022-03-07 12:05:58 +0000

    create bottom tab list component

---
+++ src/components/BottomTabList/BottomTabList.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { FlatList as RNFlatlist, FlatListProps } from 'react-native';
+import { useDispatch } from 'react-redux';
+import { toggleIsHeaderTransparent } from '../../actions/pref';
+import ListHeader from './ListHeader';
+
+interface Props extends FlatListProps<any> {
+  title: string;
+}
+
+const BottomTabList = ({ title, ...restProps }: Props) => {
+  const dispatch = useDispatch();
+
+  const [listHeaderHeight, setListHeaderHeight] = useState(0);
+
+  return (
+    <RNFlatlist
+      onScroll={({
+        nativeEvent: {
+          contentOffset: { y },
+        },
+      }) => {
+        y >= listHeaderHeight
+          ? dispatch(toggleIsHeaderTransparent(true))
+          : dispatch(toggleIsHeaderTransparent(false));
+      }}
+      ListHeaderComponent={
+        <ListHeader title={title} setListHeaderHeight={setListHeaderHeight} />
+      }
+      {...restProps}
+    />
+  );
+};
+
+export default BottomTabList;
