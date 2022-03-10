00dbe580ca601c1487e2a099cb7cc53081036cc7
Tony <toneyobaze@gmail.com>
2022-03-10 22:54:41 +0000

    create animated progressbar component

---
+++ src/components/AnimatedProgressbar.tsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+import { View, Animated, StyleSheet } from 'react-native';
+import { colors } from '../values/colors';
+
+const AnimatedView = Animated.createAnimatedComponent(View);
+
+const AnimatedProgressbar = ({ progress }) => {
+  const [progressAnimVal] = useState(new Animated.Value(0));
+
+  useEffect(() => {
+    Animated.timing(progressAnimVal, {
+      toValue: progress,
+      duration: 100,
+      useNativeDriver: false,
+    }).start();
+  }, [progress]);
+
+  const color = progressAnimVal.interpolate({
+    inputRange: [0, 50, 100],
+    outputRange: [colors.primaryRed, colors.primaryYellow, colors.primaryGreen],
+  });
+
+  const width = progressAnimVal.interpolate({
+    inputRange: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
+    outputRange: [
+      '0%',
+      '10%',
+      '20%',
+      '30%',
+      '40%',
+      '50%',
+      '60%',
+      '70%',
+      '80%',
+      '90%',
+      '100%',
+    ],
+  });
+
+  return (
+    <View style={styles.container}>
+      <AnimatedView
+        style={[styles.progress, { backgroundColor: color, width }]}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.mutedText,
+    height: 5,
+    width: '50%',
+    borderRadius: 5,
+  },
+
+  progress: {
+    borderRadius: 5,
+    flex: 1,
+  },
+});
+
+export default AnimatedProgressbar;
