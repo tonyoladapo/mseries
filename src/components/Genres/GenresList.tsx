4c37647ab93c97682bd9664cc4a48b99aae454ef
Tony <toneyobaze@gmail.com>
2022-03-10 23:51:05 +0000

    create genres list

---
+++ src/components/Genres/GenresList.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { FlatList } from 'react-native';
+import { useSelector } from 'react-redux';
+import { ReducerTypes } from '../../types/reducerTypes';
+import { Genre } from '../../types/show';
+import GenreItem from './GenreItem';
+
+interface Props {
+  addGenre: (genre: Genre) => void;
+  removeGenre: (genre: Genre) => void;
+}
+
+const GenresList = ({ addGenre, removeGenre }: Props) => {
+  const { genres } = useSelector(({ show }: ReducerTypes) => show);
+  return (
+    <FlatList
+      data={genres}
+      numColumns={3}
+      contentContainerStyle={{ alignItems: 'center' }}
+      keyExtractor={({ id, name }, index) => `${index}-${name}-${id}`}
+      renderItem={({ item }) => (
+        <GenreItem item={item} addGenre={addGenre} removeGenre={removeGenre} />
+      )}
+    />
+  );
+};
+
+export default GenresList;
