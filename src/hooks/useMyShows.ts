81cf241378136465fb752fd087718e18377bb3f9
Tony <toneyobaze@gmail.com>
2022-03-11 15:39:47 +0000

    create useMyShows hook

---
+++ src/hooks/useMyShows.ts
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+import { ReducerTypes } from './../types/reducerTypes';
+import { useSelector } from 'react-redux';
+import moment from 'moment';
+
+const useMyShows = () => {
+  const { userShows } = useSelector(({ show }: ReducerTypes) => show);
+
+  const [listData, setListData] = useState<any[]>([]);
+
+  useEffect(() => {
+    setListData([
+      {
+        title: 'Returning Shows',
+        data: userShows.filter(
+          ({ status }: any) => status === 'Returning Series',
+        ),
+      },
+      {
+        title: 'Unreleased Shows',
+
+        data: userShows.filter(
+          ({ first_air_date }: any) =>
+            moment(first_air_date).isAfter(moment()) || !first_air_date,
+        ),
+      },
+      {
+        title: 'Ended Shows',
+        data: userShows.filter(
+          ({ status, lastEpisodeToAir }: any) =>
+            status === 'Ended' ||
+            (status === 'Canceled' &&
+              moment(lastEpisodeToAir.air_date).isBefore(moment())),
+        ),
+      },
+    ]);
+  }, [userShows]);
+
+  return { listData };
+};
+
+export default useMyShows;
