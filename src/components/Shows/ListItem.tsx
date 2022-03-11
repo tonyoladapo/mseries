9cd04a61741264f46117312d02db04265d405dd0
Tony <toneyobaze@gmail.com>
2022-03-11 15:38:47 +0000

    create my shows list item component

---
+++ src/components/Shows/ListItem.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { StyleSheet, View, Image } from 'react-native';
+import { dimensions } from '../../values/dimensions';
+import Text from '../Text';
+import Pressable from '../Pressable';
+
+const ListItem = ({ item }) => {
+  return (
+    <Pressable shrinkScale={0.98} style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={{
+            uri: `https://image.tmdb.org/t/p/original${item.poster_path}`,
+          }}
+        />
+      </View>
+      <View style={styles.textContainer}>
+        <Text
+          numberOfLines={2}
+          ellipsizeMode="tail"
+          fontFamily="Bold"
+          style={styles.title}>
+          {item.name}
+        </Text>
+      </View>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: 290,
+    marginVertical: 4,
+    borderRadius: dimensions.cardBorderRadius,
+    overflow: 'hidden',
+  },
+
+  imageContainer: {
+    flex: 8,
+  },
+
+  textContainer: {
+    flex: 2,
+    justifyContent: 'center',
+    paddingHorizontal: 6,
+  },
+
+  image: {
+    height: '100%',
+    width: '100%',
+    resizeMode: 'cover',
+    borderRadius: dimensions.cardBorderRadius,
+  },
+
+  title: {},
+});
+
+export default ListItem;
