284ac3167eddc12486b3a42627c88436f710b814
Tony <toneyobaze@gmail.com>
2022-03-13 21:59:56 +0000

    render bottom tab list

--- src/screens/bottomTab/Discover.tsx
+++ src/screens/bottomTab/Discover.tsx
@@ -1,19 +1,14 @@
 import React, { useEffect } from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  FlatList,
-  Image,
-  RefreshControl,
-} from 'react-native';
+import { View, StyleSheet, RefreshControl } from 'react-native';
 import { useSelector } from 'react-redux';
 import { ReducerTypes } from '../../types/reducerTypes';
 import useDiscover from '../../hooks/useDiscover';
+import BottomTabList from '../../components/BottomTabList/BottomTabList';
+import DiscoverCategory from '../../components/Discover/DiscoverCategory';
 
 const Discover = () => {
-  const { discoverShows } = useSelector(
-    ({ discover }: ReducerTypes) => discover,
+  const { discoverShows, headerHeight } = useSelector(
+    ({ discover, pref }: ReducerTypes) => ({ ...discover, ...pref }),
   );
   const { getDiscoverShows, loading } = useDiscover();
 
@@ -23,46 +18,21 @@ const Discover = () => {
 
   return (
     <View style={styles.container}>
-      <FlatList
-        refreshControl={
-          <RefreshControl refreshing={loading} onRefresh={getDiscoverShows} />
-        }
+      <BottomTabList
+        searchbarShown
+        title="Discover"
         data={discoverShows}
         keyExtractor={({ listTitle }, index) => `${index}-${listTitle}`}
-        renderItem={({ item }) => <Item item={item} />}
+        renderItem={({ item }) => <DiscoverCategory item={item} />}
+        refreshControl={
+          <RefreshControl
+            refreshing={loading}
+            onRefresh={getDiscoverShows}
+            progressViewOffset={headerHeight}
+          />
+        }
       />
     </View>
-  );
-};
-
-const Item = ({ item: { listTitle, shows } }: any) => {
-  return (
-    <>
-      <Text>{listTitle}</Text>
-      <FlatList
-        horizontal
-        data={shows}
-        keyExtractor={({ name, id }, index) => `${index}-${name}-${id}`}
-        renderItem={({ item }) => (
-          <View
-            style={{
-              flex: 1,
-              height: 150,
-              width: 100,
-              backgroundColor: 'tomato',
-              marginHorizontal: 2,
-              borderRadius: 7,
-            }}>
-            <Image
-              source={{
-                uri: `https://image.tmdb.org/t/p/original/${item.poster_path}`,
-              }}
-              style={{ width: '100%', height: '100%' }}
-            />
-          </View>
-        )}
-      />
-    </>
   );
 };
 
