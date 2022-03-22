a9c15a2d22a94cf3fd0d8911ddc152ee312e4f6a
Tony <toneyobaze@gmail.com>
2022-03-22 10:06:19 +0000

    create show details info component

---
+++ src/components/ShowDetails/Info.tsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { colors } from '../../values/colors';
+import Text from '../Text';
+
+interface Props {
+  rating: number;
+  genre: string;
+  network: string;
+  runtime: number | string;
+}
+
+const Info = ({ rating, runtime, genre, network }: Props) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.column}>
+        <Text fontFamily="Bold" style={styles.columnTitle}>
+          RATING
+        </Text>
+        <Text fontFamily="Bold" style={styles.columnValue}>
+          {rating ? rating : 'N/A'}
+        </Text>
+      </View>
+
+      <View style={styles.column}>
+        <Text fontFamily="Bold" style={styles.columnTitle}>
+          GENRE
+        </Text>
+        <Text fontFamily="Bold" style={styles.columnValue}>
+          {genre ? genre : 'N/A'}
+        </Text>
+      </View>
+
+      <View style={styles.column}>
+        <Text fontFamily="Bold" style={styles.columnTitle}>
+          NETWORK
+        </Text>
+        <Text fontFamily="Bold" style={styles.columnValue}>
+          {network ? network : 'N/A'}
+        </Text>
+      </View>
+
+      <View style={styles.column}>
+        <Text fontFamily="Bold" style={styles.columnTitle}>
+          RUNTIME
+        </Text>
+        <Text fontFamily="Bold" style={styles.columnValue}>
+          {runtime ? `${runtime} min` : 'N/A'}
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    marginHorizontal: 16,
+    marginVertical: 8,
+  },
+
+  column: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  columnTitle: {
+    color: colors.mutedText,
+    fontSize: 12,
+    flex: 1,
+  },
+
+  columnValue: {
+    fontSize: 17,
+    flex: 9,
+    textAlign: 'center',
+  },
+});
+
+export default Info;
