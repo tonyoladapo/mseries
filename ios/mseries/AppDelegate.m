8d73f436b8e7fb2fd17062f387ecc335e2f8b3b4
Tony <toneyobaze@gmail.com>
2022-05-06 12:10:28 +0000

    add expo, expo-blur packages

--- ios/mseries/AppDelegate.m
+++ ios/mseries/AppDelegate.m
@@ -36,8 +36,8 @@ static void InitializeFlipper(UIApplication *application) {
   InitializeFlipper(application);
 #endif
 
-  RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
-  RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
+  RCTBridge *bridge = [self.reactDelegate createBridgeWithDelegate:self launchOptions:launchOptions];
+  RCTRootView *rootView = [self.reactDelegate createRootViewWithBridge:bridge
                                                    moduleName:@"mseries"
                                             initialProperties:nil];
 
@@ -48,7 +48,7 @@ static void InitializeFlipper(UIApplication *application) {
   }
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-  UIViewController *rootViewController = [UIViewController new];
+  UIViewController *rootViewController = [self.reactDelegate createRootViewController];
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
@@ -56,6 +56,7 @@ static void InitializeFlipper(UIApplication *application) {
   // [REQUIRED] Register BackgroundFetch
   [[TSBackgroundFetch sharedInstance] didFinishLaunching];
 
+  [super application:application didFinishLaunchingWithOptions:launchOptions];
   return YES;
 }
 
